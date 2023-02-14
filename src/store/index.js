443e1c7717d11fc46ec252e1725dcd69e0d873ea
Dan <78473524+dankerow@users.noreply.github.com>
2023-02-14 14:35:31 -0800

    🗑️ (stores): Removed old Vuex stores files

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-export const actions = {
-	nuxtServerInit({ dispatch }, { $cookies }) {
-		const session = $cookies.get('session')
-
-		if (!session) return dispatch('auth/logout')
-
-		return dispatch('auth/verify')
-	}
-}
