7b953278ae01ca44eb7316c0b4d0679284a90b80
Dan <78473524+dankerow@users.noreply.github.com>
2023-02-19 15:50:46 -0800

    ✨ (middleware): New auth middleware (global)

---
+++ src/middleware/auth.global.ts
@@ -0,0 +1,16 @@
+import { useAuthStore } from '@/stores/auth'
+
+export default defineNuxtRouteMiddleware((to) => {
+	if (to.meta.auth === false) {
+		return
+	}
+
+	const store = useAuthStore()
+
+	const loginRoutePath = '/auth/login'
+	const isLoggedIn = store.isLoggedIn
+
+	if (!isLoggedIn && to.path !== loginRoutePath) {
+		return navigateTo(`${loginRoutePath}?redirect=${to.path}`)
+	}
+})
