83619860ab621ac0b766130aafa3bf027e368a20
Dan <78473524+dankerow@users.noreply.github.com>
2023-02-19 15:37:30 -0800

    🐛 (components/sidebar): Style + Features: Disable nav items & Add nav item's icon

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,5 +1,134 @@
+<script setup lang="ts">
+import { useSettingsStore } from '@/stores/settings'
+
+const route = useRoute()
+const colorMode = useColorMode()
+const settings = useSettingsStore()
+const { user } = useUser()
+
+const navigation = ref({
+	categories: [
+		{
+			name: 'general',
+			nav: [
+				{
+					name: 'dashboard',
+					path: '/',
+					icon: 'ic:twotone-space-dashboard'
+				},
+				{
+					name: 'albums',
+					path: '/albums',
+					icon: 'ic:twotone-folder-copy',
+					dropdown: [
+						{
+							name: 'overview',
+							path: '/albums'
+						}
+					]
+				}
+			]
+		},
+		{
+			name: 'Analytics',
+			nav: [
+				{
+					name: 'overview',
+					path: '/analytics',
+					icon: 'ic:twotone-pie-chart'
+				}
+			]
+		},
+		{
+			name: 'administration',
+			adminOnly: true,
+			nav: [
+				{
+					name: 'Settings',
+					path: '/admin/settings',
+					icon: 'ic:twotone-settings',
+					dropdown: [
+						{
+							name: 'Overview',
+							disabled: true,
+							path: '/admin/settings'
+						}
+					]
+				},
+				{
+					name: 'Users',
+					path: '/admin/users',
+					icon: 'ic:outline-supervisor-account',
+					dropdown: [
+						{
+							name: 'Overview',
+							path: '/admin/users'
+						}
+					]
+				}
+			]
+		}
+	]
+})
+
+const pinSidenav = () => {
+	const body = document.querySelector('body')
+	settings.updateSideBar(true)
+	body?.classList.add('g-sidenav-show')
+}
+
+const unpinSidenav = () => {
+	const body = document.querySelector('body')
+	settings.updateSideBar(false)
+	body?.classList.remove('g-sidenav-show')
+}
+
+const isNavItemActive = (path: string) => {
+	return route.path === path ? true : path !== '/' && route.path.startsWith(path)
+}
+
+if (process.client) {
+	watchEffect(() => {
+		if (settings.sideBar) {
+			pinSidenav()
+		} else {
+			unpinSidenav()
+		}
+	})
+}
+
+onMounted(() => {
+	const body = document.querySelector('body')
+
+	const sidenavState = settings.isSideBarPinned
+
+	if (window.innerWidth > 1200) {
+		if (sidenavState) pinSidenav()
+		else unpinSidenav()
+	}
+
+	if (window.innerWidth < 1200) {
+		body?.classList.remove('g-sidenav-show')
+		unpinSidenav()
+
+		window.addEventListener('resize', () => {
+			if (body?.classList.contains('g-sidenav-show')) {
+				unpinSidenav()
+			}
+		})
+	}
+})
+
+onUnmounted(() => {
+	window.removeEventListener('resize', () => {})
+})
+</script>
+
 <template>
-	<aside class="sidenav">
+	<aside
+		class="sidenav shadow"
+		:class="{ 'bg-darker': colorMode.value === 'dark' }"
+	>
 		<div class="sidenav-header border-bottom">
 			<div class="navbar-brand">
 				<h2 class="navbar-brand-text">
@@ -8,16 +137,19 @@
 			</div>
 		</div>
 
-		<div class="active-user sidenav-item border-bottom d-flex flex-nowrap py-4 px-3 mb-3">
-			<div class="avatar avatar-lg bg-primary text-white rounded me-3 shadow-sm">
-				<UserIcon />
+		<div
+			class="sidenav-item d-flex flex-nowrap border-bottom py-4 mb-3"
+			:class="{ 'bg-dark': colorMode.value === 'dark', 'bg-light': colorMode.value === 'light' }"
+		>
+			<div class="icon-badge icon-shape icon-shape-darker icon-lg me-3">
+				<Icon name="ic:twotone-account-box" />
 			</div>
 			<div>
 				<span style="display: block;white-space: nowrap;text-overflow: ellipsis;">
 					{{ user.firstName }} <strong> {{ user.lastName }}</strong>
 				</span>
-				<span style="display: block;white-space: nowrap;text-overflow: ellipsis;font-size: 12px">
-					<template v-if="user.permissions.administrator">
+				<span class="rounded text-bg-primary px-1 d-inline-block text-nowrap" style="white-space: nowrap;text-overflow: ellipsis;font-size: 12px">
+					<template v-if="user.role === 'admin'">
 						Administrator
 					</template>
 					<template v-else>
@@ -27,9 +159,9 @@
 			</div>
 		</div>
 
-		<div class="px-3">
-			<template v-for="(category, index) in navigation.categories">
-				<div :key="`${category.name}-${index}`" class="sidenav-item">
+		<div class="sidemenu mb-auto">
+			<template v-for="(category, index) in navigation.categories" :key="`${category.name}-${index}`">
+				<div class="sidenav-item">
 					<span class="navbar-heading">
 						{{ category.name }}
 					</span>
@@ -45,9 +177,7 @@
 									:aria-expanded="isNavItemActive(navItem.path)"
 									aria-dropdown="true"
 								>
-									<span class="nav-link-icon">
-										<HomeIcon />
-									</span>
+									<Icon class="nav-link-icon" :name="navItem.icon" />
 									<span>
 										{{ navItem.name }}
 									</span>
@@ -56,22 +186,30 @@
 								<div :id="`${navItem.name.replace(' ', '-')}-collapse`" :class="`collapse ${isNavItemActive(navItem.path) ? 'show' : ''}`">
 									<ul class="nav-link-nav list-unstyled fw-normal pb-1 small">
 										<li v-for="(navDropdownItem, indexDropdown) in navItem.dropdown" :key="`${navItem.name}-${navDropdownItem.name}-${indexDropdown}`">
-											<NuxtLink :to="navDropdownItem.path" class="rounded">
+											<a v-if="navDropdownItem.disabled" href="#" class="nav-link disabled">
+												{{ navDropdownItem.name }}
+											</a>
+											<NuxtLink v-else :to="navDropdownItem.path" class="nav-link">
 												{{ navDropdownItem.name }}
 											</NuxtLink>
 										</li>
 									</ul>
 								</div>
 							</template>
+
 							<template v-else>
+								<a v-if="navItem.disabled" href="#" class="nav-link disabled align-items-center">
+									<Icon class="nav-link-icon" :name="navItem.icon" />
+									<span>
+										{{ navItem.name }}
+									</span>
+								</a>
 								<NuxtLink
+									v-else
 									:class="`nav-link align-items-center ${isNavItemActive(navItem.path) ? 'active' : ''}`"
-									aria-dropdown="false"
 									:to="navItem.path"
 								>
-									<span class="nav-link-icon">
-										<HomeIcon />
-									</span>
+									<Icon class="nav-link-icon" :name="navItem.icon" />
 									<span>
 										{{ navItem.name }}
 									</span>
@@ -82,129 +220,17 @@
 				</div>
 			</template>
 		</div>
+
+		<hr>
+
+		<div class="sidebottom d-flex w-100 flex-nowrap px-3 py-1">
+			<ThemeSelector />
+		</div>
 	</aside>
 </template>
 
-<script>
-export default {
-	components: {
-		HomeIcon: () => import('~/assets/icons/home.svg?inline'),
-		UserIcon: () => import('~/assets/icons/user.svg?inline')
-	},
-	data({ $store }) {
-		return {
-			user: $store.state.auth.user,
-			navigation: {
-				categories: [
-					{
-						name: 'general',
-						nav: [
-							{
-								name: 'dashboard',
-								path: '/'
-							},
-							{
-								name: 'albums',
-								path: '/albums',
-								dropdown: [
-									{
-										name: 'overview',
-										path: '/albums'
-									}
-								]
-							}
-						]
-					},
-					{
-						name: 'administration',
-						adminOnly: true,
-						nav: [
-							{
-								name: 'Settings',
-								path: '/admin/settings',
-								dropdown: [
-									{
-										name: 'Overview',
-										path: '/admin/settings'
-									}
-								]
-							},
-							{
-								name: 'Users',
-								path: '/admin/users',
-								dropdown: [
-									{
-										name: 'Overview',
-										path: '/admin/users'
-									}
-								]
-							}
-						]
-					}
-				]
-			}
-		}
-	},
-	created() {
-		this.$nuxt.$on('pinSidenav', () => {
-			this.pinSidenav()
-		})
-
-		this.$nuxt.$on('unpinSidenav', () => {
-			this.unpinSidenav()
-		})
-	},
-	beforeDestroy() {
-		this.$nuxt.$off('pinSidenav')
-		this.$nuxt.$off('unpinSidenav')
-	},
-	mounted() {
-		const body = document.querySelector('body')
-
-		const sidenavState = localStorage.getItem('sidenav-state') ? localStorage.getItem('sidenav-state') : 'pinned'
-
-		if (window.innerWidth > 1200) {
-			if (sidenavState === 'pinned') this.pinSidenav()
-			else this.unpinSidenav()
-		}
-
-		if (window.innerWidth < 1200) {
-			body.classList.remove('g-sidenav-show')
-
-			window.addEventListener('resize', () => {
-				if (body.classList.contains('g-sidenav-show')) {
-					this.unpinSidenav()
-				}
-			})
-		}
-	},
-	methods: {
-		pinSidenav() {
-			const body = document.querySelector('body')
-			this.$nuxt.$emit('toggleSidenavToggler')
-			body.classList.add('g-sidenav-show')
-
-			localStorage.setItem('sidenav-state', 'pinned')
-		},
-		unpinSidenav() {
-			const body = document.querySelector('body')
-			this.$nuxt.$emit('toggleSidenavToggler')
-			body.classList.remove('g-sidenav-show')
-
-			localStorage.setItem('sidenav-state', 'unpinned')
-		},
-		isNavItemActive(path) {
-			const route = this.$route
-			return route.path === path ? true : path !== '/' && route.path.startsWith(path)
-		}
-	}
-}
-</script>
-
 <style lang="scss" scoped>
 .sidenav {
-	background-color: rgb(245, 245, 245);
-	box-shadow: 0 0 2rem 0 rgba(136, 152, 170, .15);
 	display: flex;
 	flex-direction: column;
 	height: 100%;
@@ -234,7 +260,6 @@ export default {
 	}
 
 	.sidenav-header {
-		background-color: #5b6edb;
 		height: 68px;
 		padding: 1.5rem 0;
 		text-align: center;
@@ -242,11 +267,7 @@ export default {
 	}
 
 	.sidenav-item {
-		padding: 1rem 0;
-
-		&.bg-light-blue {
-			background: #f1f3ff;
-		}
+		padding: 1rem 1rem 0;
 	}
 
 	.navbar-brand {
@@ -255,15 +276,16 @@ export default {
 			display: inline-block;
 			font-weight: bold;
 			margin-bottom: 0;
-			text-shadow: 0 2px rgba(46, 56, 111, 0.55);
+			text-shadow: 0 2px rgba(10, 10, 10, 0.55);
 			text-transform: uppercase;
 		}
 	}
 
 	.navbar-heading {
-		font-size: .75rem;
+		font-size: .70rem;
+		font-weight: lighter;
 		letter-spacing: .04em;
-		padding: .275rem .675rem;
+		padding: .25rem .45rem;
 		text-transform: uppercase;
 	}
 }
@@ -284,7 +306,7 @@ export default {
 	text-transform: capitalize;
 
 	&:not(:last-child) {
-		margin-bottom: .475rem;
+		margin-bottom: .35rem;
 	}
 }
 
@@ -293,7 +315,7 @@ export default {
 	border-radius: 0.2rem;
 	display: flex;
 	flex-wrap: nowrap;
-	font-weight: 600;
+	font-weight: 500;
 	padding: .5rem .55rem;
 	position: relative;
 	width: 100%;
@@ -302,8 +324,13 @@ export default {
 		background: rgba(94, 114, 228, 0.1);
 	}
 
-	&.active {
+	&.active, &[aria-expanded="true"] {
 		background-color: #5e72e4;
+		color: white;
+
+		.nav-link-icon {
+			color: white;
+		}
 	}
 
 	&[aria-dropdown="true"]:after {
@@ -328,24 +355,15 @@ export default {
 			content: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='#{$toggler-color}' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M5 14l6-6-6-6'/%3e%3c/svg%3e");
 			transform: rotate(90deg);
 		}
-
-		.nav-link-icon {
-			i, svg {
-				color: white;
-			}
-		}
 	}
 
 	.nav-link-icon {
 		flex-shrink: 0;
-		margin-right: .675rem;
-
-		svg {
-			color: #5e72e4;
-			height: 1rem;
-			vertical-align: text-top;
-			width: 1rem;
-		}
+		margin-right: .5rem;
+		height: 1.25rem;
+		transition: color .3s ease-in-out;
+		vertical-align: middle;
+		width: 1.25rem;
 	}
 }
 
@@ -356,55 +374,44 @@ export default {
 
 	a {
 		display: flex;
-		padding: .5rem .5rem .5rem 2rem;
+		padding: .5rem .5rem .5rem 2.3rem;
 	}
 }
 
 .dark-mode {
 	.sidenav {
-		background-color: rgb(20, 20, 20);
-		box-shadow: 1rem 0 0 0 rgba(20, 20, 20, .15);
-
-		.sidenav-header {
-			background-color: rgb(20, 20, 20);
-		}
-
-		.active-user {
-			background-color: #1b1b1b;
+		&::-webkit-scrollbar-thumb {
+			background-color: rgb(37, 37, 37);
 		}
 	}
 
 	.nav-link {
 		&.active, &[aria-expanded="true"] {
-			background-color: #1b1b1b;
+			color: white;
+			background-color: rgba(27, 27, 27);
 		}
 
 		&[aria-dropdown="true"]:after {
 			$toggler-color: rgba(170, 170, 170, .65);
 
 			content: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='#{$toggler-color}' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M5 14l6-6-6-6'/%3e%3c/svg%3e");
-			display: inline-block;
-			line-height: 0;
-			position: absolute;
-			right: 8px;
-			transform-origin: .5em 50%;
-			transition: transform .35s ease-in-out;
-			width: 1.5em;
+		}
+
+		&[aria-expanded="true"] {
+			&:after {
+				$toggler-color: rgb(170, 170, 170);
+
+				content: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='#{$toggler-color}' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M5 14l6-6-6-6'/%3e%3c/svg%3e");
+			}
 		}
 
 		&:hover {
-			background: rgba(27, 27, 27, 0.5);
-		}
-
-		.nav-link-icon {
-			svg {
-				color: inherit;
-			}
+			background: rgba(27, 27, 27, 0.8);
 		}
 	}
 
 	.nav-link-nav {
-		background-color: rgba(27, 27, 27, 0.5);
+		background-color: rgb(23, 23, 23);
 	}
 }
 </style>
