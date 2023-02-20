9431ae720e611225cafc1cf574459d990f7783c0
Dan <78473524+dankerow@users.noreply.github.com>
2023-02-19 16:06:14 -0800

    ✨ (components): Added a theme switcher

---
+++ src/components/ThemeSelector.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+const changeTheme = () => {
+	colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+}
+</script>
+
+<template>
+	<div class="color-mode" @click="changeTheme">
+		<Icon :name="`ic:twotone-${colorMode.value === 'dark' ? 'brightness-7' : 'brightness-3'}`" />
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.color-mode {
+	align-items: center;
+	border-radius: 0.25rem;
+	cursor: pointer;
+	display: flex;
+	justify-content: center;
+	padding: 0.4rem;
+	width: max-content;
+
+	svg {
+		height: 1.25rem;
+		width: auto;
+	}
+}
+</style>
