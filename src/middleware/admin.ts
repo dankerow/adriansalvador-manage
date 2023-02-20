99fc5c249c19e22d48cfe534af13250239f56e85
Dan <78473524+dankerow@users.noreply.github.com>
2023-02-19 16:04:40 -0800

    ✨ (middleware): Added an admin navigation guard

---
+++ src/middleware/admin.ts
@@ -0,0 +1,9 @@
+import { useAuthStore } from '@/stores/auth'
+
+export default defineNuxtRouteMiddleware(() => {
+	const store = useAuthStore()
+
+	if (store.isLoggedIn && store.user?.role !== 'admin') {
+		return navigateTo('/auth/login')
+	}
+})
