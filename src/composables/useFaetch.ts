05f731167b2a30cbbaa65e0cb66d2ccdab8ca83f
Dan <78473524+dankerow@users.noreply.github.com>
2023-03-21 13:05:28 -0700

    feat: Added useFaetch (composable on top of $fetch)

---
+++ src/composables/useFaetch.ts
@@ -0,0 +1,17 @@
+import type { FetchOptions } from 'ofetch'
+import { useAuthStore } from '@/stores/auth'
+
+export default <T>(url: string, options: FetchOptions = {}): Promise<T> => {
+	const authStore = useAuthStore()
+	const config = useRuntimeConfig()
+
+	const fetchOptions = options
+	fetchOptions.headers = fetchOptions.headers || {}
+
+	if (authStore.token) fetchOptions.headers.authorization = `Bearer ${authStore.token}`
+
+	return $fetch(url, {
+		baseURL: config.public.apiBaseURL,
+		...fetchOptions
+	})
+}
