fa2c30a9f896bc99154ff51ce7fb6592a2c9fed4
Dan <78473524+dankerow@users.noreply.github.com>
2023-03-21 12:40:42 -0700

    feat(stores/albums): Better types + Changed the way data is returned by store's actions

--- src/stores/albums.ts
+++ src/stores/albums.ts
@@ -1,21 +1,27 @@
-import { defineStore } from 'pinia'
+import { defineStore, acceptHMRUpdate } from 'pinia'
 import type { Ref } from 'vue'
 
-interface AlbumFile {
-	id: string
+export interface AlbumFile {
+	url: string
+	readonly id: string
 	name: string
-	size: number
+	readonly type: string
+	readonly size: number
 	albumId: string
-	createdAt: string
-	updatedAt: string
+	album?: object
+	readonly createdAt: string
+	readonly updatedAt: string
 }
 
 export interface Album {
-	id: string
+	readonly id: string
 	name: string
+	hidden: boolean
 	nsfw: boolean
-	createdAt: string
-	updatedAt: string
+	favorite: boolean
+	featured: boolean
+	readonly createdAt: string
+	readonly updatedAt: string
 	fileCount: number
 	images: AlbumFile[]
 }
@@ -32,76 +38,120 @@ export const useAlbumsStore = defineStore('albums', () => {
 	const pages: Ref<AlbumState['pages']> = ref(0)
 	const	loading: Ref<boolean> = ref(false)
 
-	const	getAlbums = async () => {
-		loading.value = true
-		const { data: albumsD, count: countD, pages: pagesD } : { data: AlbumState['albums'], count: AlbumState['count'], pages: AlbumState['pages'] } = await useFaetch('/albums')
+	const	getAlbums = async (options?: { limit: number, page: number, setData: boolean }) => {
+		try {
+			loading.value = true
+			const { data: albumsD, count: countD, pages: pagesD } : { data: AlbumState['albums'], count: AlbumState['count'], pages: AlbumState['pages'] } = await useFaetch('/albums', { params: { limit: options?.limit, page: options?.page } })
 
-		albums.value = albumsD
-		count.value = countD
-		pages.value = pagesD
-		loading.value = false
+			if (options?.setData) {
+				albums.value = albumsD
+				count.value = countD
+				pages.value = pagesD
+			}
+
+			loading.value = false
+
+			return { data: { albums: albumsD, count: countD, pages: pagesD }, error: null }
+		} catch (e: any) {
+			return { error: e.data ? e.data.error : e }
+		}
 	}
 
 	const	getAlbum = async (id: string) => {
-		loading.value = true
-		const data: Album = await useFaetch(`/albums/${id}`)
-		loading.value = false
-		return data
+		try {
+			loading.value = true
+			const data: Album = await useFaetch(`/albums/${id}`)
+			loading.value = false
+
+			return { data, error: null }
+		} catch (e: any) {
+			return { error: e.data ? e.data.error : e }
+		}
 	}
 
-	const	addAlbum = async (name: string, nsfw: boolean, hidden: boolean) => {
-		const data: Album = await useFaetch('/albums', { method: 'post', body: { name, nsfw, hidden } })
-		albums.value.push(data)
+	const	addAlbum = async (form: { name: string, nsfw: boolean, hidden: boolean, favorite: boolean, featured: boolean }) => {
+		try {
+			const data: Album = await useFaetch('/albums', { method: 'post', body: form })
+			albums.value.push(data)
 
-		return data
+			return { data, error: null }
+		} catch (e: any) {
+			return { error: e.data ? e.data.error : e }
+		}
 	}
 
-	const	updateAlbum = async (id: string, name: string, nsfw: boolean, hidden: boolean) => {
-		const data: Album = await useFaetch(`/albums/${id}`, { method: 'put', body: { name, nsfw, hidden } })
-		const index = albums.value.findIndex((album: any) => album.id === id)
-		albums.value[index] = data
+	const	updateAlbum = async (id: string, form: { name: string, nsfw: boolean, hidden: boolean, favorite: boolean, featured: boolean }) => {
+		try {
+			const data: Album = await useFaetch(`/albums/${id}`, { method: 'put', body: form })
+			const index = albums.value.findIndex((album: any) => album.id === id)
+			albums.value[index] = data
 
-		return data
+			return { data, error: null }
+		} catch (e: any) {
+			return { error: e.data ? e.data.error : e }
+		}
 	}
 
 	const	deleteAlbum = async (id: string) => {
-		await useFaetch(`/albums/${id}`, { method: 'delete' })
-		const index = albums.value.findIndex((album: any) => album.id === id)
-		albums.value.splice(index, 1)
+		try {
+			await useFaetch(`/albums/${id}`, { method: 'delete' })
+			const index = albums.value.findIndex((album: any) => album.id === id)
+			albums.value.splice(index, 1)
+
+			return { error: null }
+		} catch (e: any) {
+			return { error: e.data ? e.data.error : e }
+		}
 	}
 
 	const getAlbumFiles = async (id: string, limit = 25) => {
-		const { images }: { images: AlbumFile[] } = await useFaetch(`/albums/${id}?includeImages=true`, { params: { limit } })
-		return images
+		const { images }: { images: { data: AlbumFile[], count: number, pages: number } } = await useFaetch(`/albums/${id}?includeImages=true`, { params: { limit } })
+		images.data = images.data.map((image: any) => {
+			image.url = `/albums/${id}/files/${image.id}`
+			return image
+		})
+
+		return { images: images.data, count: images.count, pages: images.pages }
 	}
 
 	const	addFiles = async (id: string, files: File[]) => {
-		const data: Album['images'] = await useFaetch(`/albums/${id}/files`, { method: 'post', body: { files } })
-		const index = albums.value.findIndex((album: any) => album.id === id)
-		albums.value[index].images = data
+		try {
+			const data: Album['images'] = await useFaetch(`/albums/${id}/files`, { method: 'post', body: { files } })
 
-		return data
+			return { data, error: null }
+		} catch (e: any) {
+			return { error: e.data ? e.data.error : e }
+		}
 	}
 
-	const	removeFile = async (id: string, fileId: string) => {
-		await useFaetch(`/albums/${id}/files/${fileId}`, { method: 'delete' })
-		const index = albums.value.findIndex((album: any) => album.id === id)
-		const fileIndex = albums.value[index].images.findIndex((file: any) => file.id === fileId)
-		albums.value[index].images.splice(fileIndex, 1)
+	const	deleteFile = async (id: string) => {
+		try {
+			await useFaetch(`/images/${id}`, { method: 'delete' })
+
+			return { error: null }
+		} catch (e: any) {
+			return { error: e.data ? e.data.error : e }
+		}
 	}
 
-	const	updateFile = async (id: string, fileId: string, name: string, nsfw: boolean, hidden: boolean) => {
-		const data: AlbumFile = await useFaetch(`/albums/${id}/files/${fileId}`, { method: 'put', body: { name, nsfw, hidden } })
-		const index = albums.value.findIndex((album: any) => album.id === id)
-		const fileIndex = albums.value[index].images.findIndex((file: any) => file.id === fileId)
-		albums.value[index].images[fileIndex] = data
+	const	updateFile = async (id: string, form: AlbumFile) => {
+		try {
+			const data: AlbumFile = await useFaetch(`/images/${id}`, { method: 'put', body: form })
 
-		return data
+			return { data, error: null }
+		} catch (e: any) {
+			return { error: e.data ? e.data.error : e }
+		}
 	}
 
-	const	getFile = async (id: string, fileId: string) => {
-		const data: AlbumFile = await useFaetch(`/albums/${id}/files/${fileId}`)
-		return data
+	const	getFile = async (id: string) => {
+		try {
+			const data: AlbumFile = await useFaetch(`/images/${id}`)
+
+			return { data, error: null }
+		} catch (e: any) {
+			return { error: e.data ? e.data.error : e }
+		}
 	}
 
 	return {
@@ -116,8 +166,12 @@ export const useAlbumsStore = defineStore('albums', () => {
 		deleteAlbum,
 		getAlbumFiles,
 		addFiles,
-		removeFile,
+		deleteFile,
 		updateFile,
 		getFile
 	}
 })
+
+if (import.meta.hot) {
+	import.meta.hot.accept(acceptHMRUpdate(useAlbumsStore, import.meta.hot))
+}
