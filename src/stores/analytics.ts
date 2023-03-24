9001790973aa504b41a8a55928f887bbfcc443e8
Dan <78473524+dankerow@users.noreply.github.com>
2023-03-24 03:05:28 -0700

    feat: add analytics store (wip)

---
+++ src/stores/analytics.ts
@@ -0,0 +1,43 @@
+import type { Ref } from 'vue'
+
+export const useAnalyticsStore = defineStore('analytics', () => {
+	const pageViews: Ref<number> = ref(0)
+	const newVisitors: Ref<number> = ref(0)
+	const totalVisitors: Ref<number> = ref(0)
+	const engagementRate: Ref<number> = ref(0)
+	const popularPages: Ref<Array<string>> = ref([])
+	const trendingPages: Ref<Array<string>> = ref([])
+	const loading: Ref<boolean> = ref(false)
+
+	const fetchAnalytics = async () => {
+		try {
+			loading.value = true
+
+			const data = await useFaetch('/analytics')
+
+			pageViews.value = data.basic.pageViews
+			totalVisitors.value = data.basic.totalVisitors
+			newVisitors.value = data.basic.newVisitors
+			engagementRate.value = data.basic.engagementRate
+			popularPages.value = data.popular
+			trendingPages.value = data.trending
+
+			loading.value = false
+
+			return { data, error: null }
+		} catch (e: any) {
+			return { error: e.data ? e.data.error : e }
+		}
+	}
+
+	return {
+		pageViews,
+		totalVisitors,
+		newVisitors,
+		engagementRate,
+		popularPages,
+		trendingPages,
+		loading,
+		fetchAnalytics
+	}
+})
