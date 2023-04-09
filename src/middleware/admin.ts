7525a7cc372c4d2a7247c6c3a331e71f64cdffa4
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-08 17:58:20 -0700

    refactor: lint

--- src/middleware/admin.ts
+++ src/middleware/admin.ts
@@ -1,9 +1,9 @@
 import { useAuthStore } from '@/stores/auth'
 
 export default defineNuxtRouteMiddleware(() => {
-	const store = useAuthStore()
+  const store = useAuthStore()
 
-	if (store.isLoggedIn && store.user?.role !== 'admin') {
-		return navigateTo('/auth/login')
-	}
+  if (store.isLoggedIn && store.user?.role !== 'admin') {
+    return navigateTo('/auth/login')
+  }
 })
