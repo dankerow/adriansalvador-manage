c2cca9fdace0d624ea224baf8823cfdc2f176509
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-08 17:40:17 -0700

    refactor: lint

--- src/plugins/color-mode.ts
+++ src/plugins/color-mode.ts
@@ -1,11 +1,11 @@
 export default defineNuxtPlugin(() => {
-	const colorMode = useColorMode()
+  const colorMode = useColorMode()
 
-	useHead({
-		meta: [{
-			id: 'theme-color',
-			name: 'theme-color',
-			content: () => colorMode.value === 'dark' ? '#1b1b1b' : '#ffffff'
-		}]
-	})
+  useHead({
+    meta: [{
+      id: 'theme-color',
+      name: 'theme-color',
+      content: () => colorMode.value === 'dark' ? '#1b1b1b' : '#ffffff'
+    }]
+  })
 })
