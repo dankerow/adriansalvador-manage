c5e108d72cd467b23c5279f68d6d1a20f1be4793
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-10 01:15:31 -0700

    feat: types + new deleteUsers method + lint

--- src/stores/users.ts
+++ src/stores/users.ts
@@ -1,76 +1,75 @@
 import type { Ref } from 'vue'
-
-interface User {
-	readonly id: string
-	username: string
-	email: string
-	readonly createdAt: string
-	readonly updatedAt: string
-}
-
-interface UserState {
-	users: (User)[]
-	count: number
-	pages: number
-}
+import type { User } from '@/types/User'
 
 export const useUsersStore = defineStore('users', () => {
-	const users: Ref<UserState['users']> = ref([])
-	const count: Ref<UserState['count']> = ref(0)
-	const pages: Ref<UserState['pages']> = ref(0)
-	const loading: Ref<boolean> = ref(false)
+  const users: Ref<User[]> = ref([])
+  const count: Ref<number> = ref(0)
+  const pages: Ref<number> = ref(0)
+  const loading: Ref<boolean> = ref(false)
 
-	const getUsers = async () => {
-		loading.value = true
-		const { data: usersD, count: countD, pages: pagesD } : { data: UserState['users'], count: UserState['count'], pages: UserState['pages'] } = await useFaetch('/users')
+  const getUsers = async () => {
+    loading.value = true
+    const { data: usersD, count: countD, pages: pagesD } : { data: User[], count: number, pages: number } = await useFaetch('/users')
 
-		users.value = usersD
-		count.value = countD
-		pages.value = pagesD
-		loading.value = false
-	}
+    users.value = usersD
+    count.value = countD
+    pages.value = pagesD
+    loading.value = false
+  }
 
-	const getUser = async (id: string) => {
-		loading.value = true
-		const data: User = await useFaetch(`/users/${id}`)
-		loading.value = false
-		return data
-	}
+  const getUser = async (id: string) => {
+    loading.value = true
+    const data: User = await useFaetch(`/users/${id}`)
+    loading.value = false
+    return data
+  }
 
-	const addUser = async ({ firstName, lastName, email }: { firstName: string, lastName: string, email: string }) => {
-		try {
-			const data: User = await useFaetch('/users', { method: 'post', body: { firstName, lastName, email } })
-			users.value.push(data)
+  const addUser = async ({ firstName, lastName, email }: { firstName: string, lastName: string, email: string }) => {
+    try {
+      const data: User = await useFaetch('/users', { method: 'post', body: { firstName, lastName, email } })
+      users.value.push(data)
 
-			return { error: null }
-		} catch (e: any) {
-			return { error: e.data ? e.data.error : e }
-		}
-	}
+      return { error: null }
+    } catch (e: any) {
+      return { error: e.data ? e.data.error : e }
+    }
+  }
 
-	const updateUser = async (id: string, username: string, email: string, password: string) => {
-		const data: User = await useFaetch(`/users/${id}`, { method: 'put', body: { username, email, password } })
-		const index = users.value.findIndex((user: any) => user.id === id)
-		users.value[index] = data
+  const updateUser = async (id: string, username: string, email: string, password: string) => {
+    const data: User = await useFaetch(`/users/${id}`, { method: 'put', body: { username, email, password } })
+    const index = users.value.findIndex((user: any) => user.id === id)
+    users.value[index] = data
 
-		return data
-	}
+    return data
+  }
 
-	const deleteUser = async (id: string) => {
-		await useFaetch(`/users/${id}`, { method: 'delete' })
-		const index = users.value.findIndex((user: any) => user.id === id)
-		users.value.splice(index, 1)
-	}
+  const deleteUser = async (id: string) => {
+    await useFaetch(`/users/${id}`, { method: 'delete' })
+    const index = users.value.findIndex((user: any) => user.id === id)
+    users.value.splice(index, 1)
+  }
 
-	return {
-		users,
-		count,
-		pages,
-		loading,
-		getUsers,
-		getUser,
-		addUser,
-		updateUser,
-		deleteUser
-	}
+  const deleteUsers = async (ids: string[]) => {
+    try {
+      await useFaetch('/users', { method: 'delete', body: { ids } })
+      users.value = users.value.filter((user: any) => !ids.includes(user.id))
+
+      return { error: null }
+    } catch (e: any) {
+      return { error: e.data ? e.data.error : e }
+    }
+  }
+
+  return {
+    users,
+    count,
+    pages,
+    loading,
+    getUsers,
+    getUser,
+    addUser,
+    updateUser,
+    deleteUser,
+    deleteUsers
+  }
 })
