b600a8ec3da3c65301e9ace0d6a44ba02aa9b56c
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-10 01:19:26 -0700

    chore(dep): remove sass-loader + add @nuxtjs/critters

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,81 +2,87 @@ const isDevelopment = process.env.NODE_ENV === 'development'
 const isProduction = process.env.NODE_ENV === 'production'
 
 export default defineNuxtConfig({
-	rootDir: './',
-	srcDir: 'src',
+  rootDir: './',
+  srcDir: 'src',
 
-	debug: isDevelopment,
-	sourcemap: isDevelopment,
+  debug: isDevelopment,
+  sourcemap: isDevelopment,
 
-	nitro: {
-		prerender: {
-			crawlLinks: true
-		}
-	},
+  typescript: {
+    strict: true
+  },
 
-	typescript: {
-		strict: true
-	},
+  runtimeConfig: {
+    public: {
+      apiBaseURL: isProduction ? process.env.API_BASE_URL : process.env.API_BASE_URL_DEV,
+      cdnBaseURL: isProduction ? process.env.CDN_BASE_URL : process.env.CDN_BASE_URL_DEV
+    }
+  },
 
-	runtimeConfig: {
-		public: {
-			apiBaseURL: isProduction ? process.env.API_BASE_URL : process.env.API_BASE_URL_DEV,
-			cdnBaseURL: isProduction ? process.env.CDN_BASE_URL : process.env.CDN_BASE_URL_DEV
-		}
-	},
+  css: [
+    '@/assets/scss/bedrock.scss'
+  ],
 
-	css: [
-		'@/assets/scss/bedrock.scss'
-	],
+  hooks: {
+    'pages:extend': (pages) => {
+      pages.forEach((page) => {
+        if (page.path === '/dashboard') {
+          Object.assign(page, { name: 'dashboard', path: '/' })
+        }
+      })
+    }
+  },
 
-	hooks: {
-		'pages:extend': (pages) => {
-			pages.forEach((page) => {
-				if (page.path === '/dashboard') {
-					Object.assign(page, { name: 'dashboard', path: '/' })
-				}
-			})
-		}
-	},
+  modules: [
+    '@nuxt/devtools',
+    '@nuxt/image-edge',
+    '@nuxtjs/color-mode',
+    '@nuxtjs/critters',
+    '@nuxtjs/google-fonts',
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    'nuxt-icon',
+    'nuxt-purgecss',
+    'nuxt-time',
+    'nuxt-security'
+  ],
 
-	modules: [
-		'@nuxt/devtools',
-		'@nuxt/image-edge',
-		'@nuxtjs/color-mode',
-		'@nuxtjs/google-fonts',
-		'@pinia/nuxt',
-		'@vueuse/nuxt',
-		'nuxt-icon',
-		'nuxt-purgecss',
-		'nuxt-time'
-	],
+  colorMode: {
+    preference: 'system',
+    fallback: 'dark'
+  },
 
-	colorMode: {
-		preference: 'system',
-		fallback: 'dark'
-	},
+  googleFonts: {
+    families: {
+      Archivo: {
+        wght: [400, 500, 600, 700, 800, 900]
+      }
+    },
+    display: 'swap'
+  },
 
-	googleFonts: {
-		families: {
-			Archivo: {
-				wght: [400, 500, 600, 700, 800, 900]
-			}
-		},
-		display: 'swap'
-	},
+  image: {
+    domains: [isProduction ? process.env.CDN_BASE_URL as string : process.env.CDN_BASE_URL_DEV as string]
+  },
 
-	image: {
-		domains: [isProduction ? process.env.CDN_BASE_URL : process.env.CDN_BASE_URL_DEV]
-	},
+  pinia: {
+    autoImports: [
+      'defineStore',
+      'acceptHMRUpdate'
+    ]
+  },
 
-	pinia: {
-		autoImports: [
-			'defineStore',
-			'acceptHMRUpdate'
-		]
-	},
+  purgecss: {
+    safelist: [
+      'dark-mode',
+      'svg',
+      /^btn-/,
+      /^dropdown/,
+      'sidenav'
+    ]
+  },
 
-	purgecss: {
-		variables: true
-	}
+  security: {
+    enabled: isProduction
+  }
 })
