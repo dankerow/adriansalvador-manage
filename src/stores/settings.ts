bea8c249c047461a6f3c9f0b94565890808555d5
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-10 01:14:25 -0700

    refactor: lint + auto import acceptHMRUpdate & defineStore

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -1,30 +1,29 @@
-import { acceptHMRUpdate, defineStore } from 'pinia'
 import type { Ref } from 'vue'
 
 export const useSettingsStore = defineStore('settings', () => {
-	const	sideBar: Ref<boolean> = ref(false)
+  const	sideBar: Ref<boolean> = ref(false)
 
-	const isSideBarPinned = computed(() => sideBar.value)
+  const isSideBarPinned = computed(() => sideBar.value)
 
-	const	toggleSideBar = () => {
-		sideBar.value = !sideBar.value
+  const	toggleSideBar = () => {
+    sideBar.value = !sideBar.value
 
-		const sidebarCookie = useCookie('sidebar')
-		sidebarCookie.value = String(sideBar.value)
-	}
+    const sidebarCookie = useCookie('sidebar')
+    sidebarCookie.value = String(sideBar.value)
+  }
 
-	const updateSideBar = (newSideBar: boolean) => {
-		sideBar.value = newSideBar
-	}
+  const updateSideBar = (newSideBar: boolean) => {
+    sideBar.value = newSideBar
+  }
 
-	return {
-		sideBar,
-		isSideBarPinned,
-		toggleSideBar,
-		updateSideBar
-	}
+  return {
+    sideBar,
+    isSideBarPinned,
+    toggleSideBar,
+    updateSideBar
+  }
 })
 
 if (import.meta.hot) {
-	import.meta.hot.accept(acceptHMRUpdate(useSettingsStore, import.meta.hot))
+  import.meta.hot.accept(acceptHMRUpdate(useSettingsStore, import.meta.hot))
 }
