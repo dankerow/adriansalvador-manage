3aed4f899205caa9e6c1f3fbe49d578d545ecfa8
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-10 01:18:14 -0700

    feat: user types

---
+++ src/types/User.ts
@@ -0,0 +1,9 @@
+export interface User {
+  readonly id: string
+  firstName: string
+  lastName: string
+  email: string
+  role: string
+  readonly createdAt: string
+  readonly updatedAt: string
+}
