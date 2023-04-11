1aea9d03940c150903bbab80eaa2f29ceb428802
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-11 01:16:48 -0700

    feat: add image count(+lint)

--- src/stores/analytics.ts
+++ src/stores/analytics.ts
@@ -1,43 +1,46 @@
 import type { Ref } from 'vue'
 
 export const useAnalyticsStore = defineStore('analytics', () => {
-	const pageViews: Ref<number> = ref(0)
-	const newVisitors: Ref<number> = ref(0)
-	const totalVisitors: Ref<number> = ref(0)
-	const engagementRate: Ref<number> = ref(0)
-	const popularPages: Ref<Array<string>> = ref([])
-	const trendingPages: Ref<Array<string>> = ref([])
-	const loading: Ref<boolean> = ref(false)
+  const pageViews: Ref<number> = ref(0)
+  const newVisitors: Ref<number> = ref(0)
+  const totalVisitors: Ref<number> = ref(0)
+  const engagementRate: Ref<number> = ref(0)
+  const imageCount: Ref<number> = ref(0)
+  const popularPages: Ref<Array<string>> = ref([])
+  const trendingPages: Ref<Array<string>> = ref([])
+  const loading: Ref<boolean> = ref(false)
 
-	const fetchAnalytics = async () => {
-		try {
-			loading.value = true
+  const fetchAnalytics = async () => {
+    try {
+      loading.value = true
 
-			const data = await useFaetch('/analytics')
+      const data = await useFaetch('/analytics')
 
-			pageViews.value = data.basic.pageViews
-			totalVisitors.value = data.basic.totalVisitors
-			newVisitors.value = data.basic.newVisitors
-			engagementRate.value = data.basic.engagementRate
-			popularPages.value = data.popular
-			trendingPages.value = data.trending
+      pageViews.value = data.basic.pageViews
+      totalVisitors.value = data.basic.totalVisitors
+      newVisitors.value = data.basic.newVisitors
+      engagementRate.value = data.basic.engagementRate
+      imageCount.value = data.imageCount
+      popularPages.value = data.popular
+      trendingPages.value = data.trending
 
-			loading.value = false
+      loading.value = false
 
-			return { data, error: null }
-		} catch (e: any) {
-			return { error: e.data ? e.data.error : e }
-		}
-	}
+      return { data, error: null }
+    } catch (e: any) {
+      return { error: e.data ? e.data.error : e }
+    }
+  }
 
-	return {
-		pageViews,
-		totalVisitors,
-		newVisitors,
-		engagementRate,
-		popularPages,
-		trendingPages,
-		loading,
-		fetchAnalytics
-	}
+  return {
+    pageViews,
+    totalVisitors,
+    newVisitors,
+    engagementRate,
+    imageCount,
+    popularPages,
+    trendingPages,
+    loading,
+    fetchAnalytics
+  }
 })
