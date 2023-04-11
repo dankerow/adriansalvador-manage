7eb759ebe78069388d2a0b58adc5c52c3491b911
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-11 01:25:41 -0700

    feat: add album overview parent page

---
+++ src/pages/dashboard/albums/[id].vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { Album } from '@/types/Album'
+
+import { useAlbumsStore } from '@/stores/albums'
+
+const route = useRoute()
+const albumsStore = useAlbumsStore()
+
+const album: Album = albumsStore.albums.find(album => album.id === route.params.id) || (await albumsStore.getAlbum(route.params.id)).data
+
+const deleteAlbum = async () => {
+  const { error } = await albumsStore.deleteAlbum(album.id)
+
+  if (!error) {
+    return navigateTo('/albums')
+  }
+}
+
+const isRootPath = computedEager(() => route.name?.startsWith('dashboard-albums-id') && !route.name?.includes('files'))
+</script>
+
+<template>
+  <div>
+    <Banner
+      v-if="isRootPath"
+      :links="[{ name: 'Albums', path: '/albums' }, { name: album.name, path: `/albums/${album.id}` }]"
+      icon="ic:twotone-folder"
+      :buttons="[
+        { name: 'upload', text: 'Upload images', url: `/albums/${album.id}/upload`, icon: 'ic:twotone-upload' },
+        { name: 'edit', text: 'Edit', url: `/albums/${album.id}/edit`, icon: 'ic:twotone-edit' },
+        { name: 'delete', text: 'Delete', callback: deleteAlbum, icon: 'ic:twotone-delete' }
+      ]"
+    />
+
+    <NuxtPage />
+  </div>
+</template>
