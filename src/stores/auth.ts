1b6e0fed2ac518eb396c0168cebc76435e738772
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-21 20:07:24 -0700

    feat: move User interface to types folder (+lint)

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,86 +1,77 @@
 import type { Ref } from 'vue'
-
-interface User {
-	readonly id: string
-	firstName: string
-	lastName: string
-	email: string
-	role: string
-	readonly createdAt: string
-	readonly updatedAt: string
-}
+import type { User } from '@/types/User'
 
 export const useAuthStore = defineStore('auth', () => {
-	const user: Ref<User|null> = ref(null)
-	const token: Ref<string|null> = ref(null)
+  const user: Ref<User|null> = ref(null)
+  const token: Ref<string|null> = ref(null)
 
-	const isLoggedIn = computed(() => {
-		return !!user.value
-	})
+  const isLoggedIn = computed(() => {
+    return !!user.value
+  })
 
-	const verify = async () => {
-		try {
-			const data: User = await useFaetch('/authentication/verify')
-			if (!data && !isLoggedIn) return logout()
+  const verify = async () => {
+    try {
+      const data: User = await useFaetch('/authentication/verify')
+      if (!data && !isLoggedIn) return logout()
 
-			user.value = data
-			return { error: null }
-		} catch (e: any) {
-			return { error: e.data ? e.data.error : e }
-		}
-	}
+      user.value = data
+      return { error: null }
+    } catch (e: any) {
+      return { error: e.data ? e.data.error : e }
+    }
+  }
 
-	const login = async ({ email, password }: { email: string, password: string }) => {
-		try {
-			const data: { user: User, token: string } = await useFaetch('/authentication/login', { method: 'post', body: { email, password } })
+  const login = async ({ email, password }: { email: string, password: string }) => {
+    try {
+      const data: { user: User, token: string } = await useFaetch('/authentication/login', { method: 'post', body: { email, password } })
 
-			token.value = data.token
-			user.value = data.user
+      token.value = data.token
+      user.value = data.user
 
-			const tokenCookie = useCookie<string>('token')
-			tokenCookie.value = data.token
+      const tokenCookie = useCookie<string>('token')
+      tokenCookie.value = data.token
 
-			return { error: null }
-		} catch (e: any) {
-			return { error: e.data ? e.data.error : e }
-		}
-	}
+      return { error: null }
+    } catch (e: any) {
+      return { error: e.data ? e.data.error : e }
+    }
+  }
 
-	const logout = () => {
-		user.value = null
-		token.value = null
+  const logout = () => {
+    user.value = null
+    token.value = null
 
-		const tokenCookie = useCookie<string>('token')
-		tokenCookie.value = null
-	}
+    const tokenCookie = useCookie<string>('token')
+    tokenCookie.value = ''
+  }
 
-	const updateUser = async (newUser: User) => {
-		try {
-			const data: User = await useFaetch('/@me', { method: 'put', body: newUser })
-			user.value = data
+  const updateUser = async (newUser: User) => {
+    try {
+      const data: User = await useFaetch('/@me', { method: 'put', body: newUser })
+      user.value = data
 
-			return { data, error: null }
-		} catch (e: any) {
-			return { error: e.data ? e.data.error : e }
-		}
-	}
+      return { data, error: null }
+    } catch (e: any) {
+      return { error: e.data ? e.data.error : e }
+    }
+  }
 
-	const updateToken = (newToken: string) => {
-		token.value = newToken
-	}
+  const updateToken = (newToken: string) => {
+    token.value = newToken
+  }
 
-	return {
-		user,
-		token,
-		isLoggedIn,
-		verify,
-		login,
-		logout,
-		updateToken,
-		updateUser
-	}
+  return {
+    user,
+    token,
+    isLoggedIn,
+    verify,
+    login,
+    logout,
+    updateToken,
+    updateUser
+  }
 })
 
 if (import.meta.hot) {
-	import.meta.hot.accept(acceptHMRUpdate(useAuthStore, import.meta.hot))
+  import.meta.hot.accept(acceptHMRUpdate(useAuthStore, import.meta.hot))
 }
