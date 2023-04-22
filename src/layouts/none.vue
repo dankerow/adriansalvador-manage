00cb248f07b9cae086964864b7b1897dd53d901b
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-21 20:10:04 -0700

    feat: add 'none' layout

---
+++ src/layouts/none.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+    dir: 'auto'
+  },
+  title: null,
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - Adrian Salvador - Dashboard` : 'Adrian Salvador - Dashboard'
+  },
+  meta: [
+    { name: 'description', content: '' },
+    { name: 'robots', content: 'noydir,noodp,noarchive,nocache,notranslate,nofollow,noimageindex,noindex' }
+  ],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    { rel: 'preconnect', href: 'https://cdn.salvadoradrian.com' },
+    { rel: 'dns-prefetch', href: 'https://cdnjs.cloudflare.com' }
+  ],
+  script: [
+    {
+      src: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0-alpha3/js/bootstrap.bundle.min.js',
+      type: 'text/javascript',
+      defer: true
+    }
+  ]
+})
+
+onMounted(() => {
+  const setTheme = function(theme: string) {
+    document.documentElement.setAttribute('data-bs-theme', theme)
+  }
+  setTheme(colorMode.value)
+})
+</script>
+
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
