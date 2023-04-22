07b8b38c8af34e74daeead32bf8443c231688bf0
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-21 20:11:39 -0700

    feat: default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import { useSettingsStore } from '@/stores/settings'
+
+const settings = useSettingsStore()
+
 const colorMode = useColorMode()
 
 useHead({
@@ -12,7 +16,6 @@ useHead({
   },
   meta: [
     { name: 'description', content: '' },
-    { name: 'format-detection', content: 'telephone=no' },
     { name: 'robots', content: 'noydir,noodp,noarchive,nocache,notranslate,nofollow,noimageindex,noindex' }
   ],
   link: [
@@ -34,11 +37,145 @@ onMounted(() => {
     document.documentElement.setAttribute('data-bs-theme', theme)
   }
   setTheme(colorMode.value)
+
+  const backdrop = document.querySelector('#sidebar-backdrop') as HTMLElement
+
+  backdrop.addEventListener('click', () => {
+    settings.toggleSidebar()
+  })
 })
 </script>
 
 <template>
-  <div>
-    <slot />
+  <div class="d-flex flex-row align-items-stretch">
+    <SideBar />
+
+    <div class="main-content" role="main">
+      <div id="sidebar-backdrop" class="sidebar-backdrop" />
+
+      <Header />
+
+      <slot />
+    </div>
+
+    <div class="toast-container position-fixed bottom-0 end-0 p-3" style="z-index: 99;" />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.main-content {
+  --bs-bg-opacity: 1;
+
+  background-color: rgba(var(--bs-light-subtle-rgb), var(--bs-bg-opacity));
+  height: 100%;
+  overflow-x: hidden;
+  padding-bottom: 2rem;
+  position: fixed;
+  transition: padding-left .3s ease-in-out;
+  width: 100%;
+
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: #abb7ff;
+  }
+
+  &::-webkit-scrollbar-button {
+    display: none;
+  }
+
+  &::-webkit-scrollbar-corner {
+    background: transparent;
+  }
+}
+
+@media (min-width: 768px) {
+  .main-content .container,
+  .main-content .container-fluid,
+  .main-content .container-md,
+  .main-content .container-sm,
+  .main-content .container-lg,
+  .main-content .container-xl {
+    padding-right: 40px;
+    padding-left: 40px;
+  }
+}
+
+.main-content .sidebar-backdrop {
+  --bs-bg-opacity: 1;
+
+  background-color: rgba(var(--bs-black-rgb), var(--bs-bg-opacity));
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 998;
+  opacity: .5;
+  display: none;
+}
+
+.g-sidebar-show {
+  .sidebar {
+    left: 0;
+
+    .navbar-brand, .navbar-heading {
+      display: block;
+    }
+
+    .nav-item {
+      .collapse {
+        height: auto;
+        transition: all .3s ease-in-out;
+      }
+    }
+
+    .nav-link-text {
+      transition: .3s ease-in-out;
+      opacity: 1;
+    }
+  }
+}
+
+@media screen and (min-width: 768px) {
+  .g-sidebar-show {
+    .main-content {
+      padding-left: 250px;
+    }
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .g-sidebar-show {
+    .main-content {
+      .sidebar-backdrop {
+        display: block;
+      }
+    }
+  }
+}
+
+@media screen and (min-width: 768px) {
+  .g-sidebar-show {
+    &.pinned {
+      .main-content {
+        padding-left: 80px;
+      }
+    }
+  }
+}
+
+.dark-mode {
+  .main-content {
+    background-color: transparent;
+
+    &::-webkit-scrollbar-thumb {
+      --bs-bg-opacity: 1;
+
+      background-color: rgba(var(--bs-dark-rgb), var(--bs-bg-opacity));
+    }
+  }
+}
+</style>
