ed436b65c17d09a5a1eb8485bc494f77d27d9e12
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-21 20:10:16 -0700

    feat: add main page

---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'none',
+  middleware: [
+    function () {
+      return navigateTo('/dashboard')
+    }
+  ]
+})
+</script>
+
+<template>
+  <div />
+</template>
