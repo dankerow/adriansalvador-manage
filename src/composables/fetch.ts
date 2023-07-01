a691e54cde5475fedd8ebd8349ccabe1dde29fb1
Dan <danker.twist@gmail.com>
2023-06-30 22:09:10 -0700

    feat: refactor fetch functionalities into one file
    Files useFaetch.ts and useFutch.ts were deleted and their functionalities were moved into a new file called fetch.ts. This improves maintainability of the code by keeping all fetching related logic in one place. In addition, withLoadingState has been added to handle loading states during fetches.

---
+++ src/composables/fetch.ts
@@ -0,0 +1,48 @@
+import type { Ref } from 'vue'
+import { UseFetchOptions } from '#app'
+import { NitroFetchOptions } from 'nitropack'
+import defu from 'defu'
+
+export const withLoadingState = async (loading: Ref<boolean>, fetchFn: () => Promise<any>) => {
+  try {
+    loading.value = true
+    const result = await fetchFn()
+    return result
+  } catch (e: any) {
+    throw e.data ? e.data.error : e
+  } finally {
+    loading.value = false
+  }
+}
+
+export const useFaetch = <T>(url: string, options: NitroFetchOptions<Request> = {}): Promise<T> => {
+  const userAuth = useCookie('token')
+  const config = useRuntimeConfig()
+
+  const defaults: NitroFetchOptions<Request> = {
+    baseURL: config.public.apiBaseURL,
+    headers: userAuth.value
+      ? { Authorization: `Bearer ${userAuth.value}` }
+      : {}
+  }
+
+  const params = defu(options, defaults)
+
+  return $fetch(url, params)
+}
+
+export const useFutch = <T>(url: string, options: UseFetchOptions<T> = {}) => {
+  const userAuth = useCookie('token')
+  const config = useRuntimeConfig()
+
+  const defaults: UseFetchOptions<T> = {
+    baseURL: config.public.apiBaseURL,
+    headers: userAuth.value
+      ? { Authorization: `Bearer ${userAuth.value}` }
+      : {}
+  }
+
+  const params = defu(options, defaults)
+
+  return useFetch(url, params)
+}
