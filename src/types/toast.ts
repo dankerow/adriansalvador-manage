e9b90cf1d65b90c19ee083cab0731d8759a2df3d
Dan <danker.twist@gmail.com>
2023-07-31 17:17:19 -0700

    feat: add Toast interface in types

---
+++ src/types/toast.ts
@@ -0,0 +1,10 @@
+export interface Toast {
+  autohide?: boolean,
+  color?: string,
+  delay?: number,
+  dismissible?: boolean,
+  index: number,
+  title: string,
+  body: string,
+  visible?: boolean,
+}
