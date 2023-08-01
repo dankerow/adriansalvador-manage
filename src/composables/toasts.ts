eab067772da4c516257f6f62960d539aa3fc8c36
Dan <danker.twist@gmail.com>
2023-07-31 17:18:37 -0700

    feat: replace generic object with Toast type

--- src/composables/toasts.ts
+++ src/composables/toasts.ts
@@ -1,13 +1,15 @@
-export const useToasts = () => useState<object[]>('toasts', () => [])
+import type { Toast } from '@/types/toast'
+
+export const useToasts = () => useState<Toast[]>('toasts', () => [])
 
 export const useToast = () => {
   const toasts = useToasts()
 
-  const addToast = (toast: object) => {
+  const addToast = (toast: Toast) => {
     toasts.value.push(toast)
   }
 
-  const removeToast = (toast: object) => {
+  const removeToast = (toast: Toast) => {
     const index = toasts.value.indexOf(toast)
     if (index > -1) toasts.value.splice(index, 1)
   }
