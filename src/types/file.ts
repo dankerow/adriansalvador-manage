630d5bf8128211865d5f57ae5ff94b74d2052dd8
Dan <danker.twist@gmail.com>
2023-11-12 02:09:47 -0500

    chore: add file types

---
+++ src/types/file.ts
@@ -0,0 +1,19 @@
+import type { Album } from './album'
+
+export interface File {
+  readonly id: string
+  name: string
+  url?: string
+  readonly type: string
+  readonly size: number
+  albumId?: string
+  album?: Album
+  metadata: FileMetadata
+  readonly createdAt: number
+  readonly modifiedAt: number
+}
+
+export interface FileMetadata {
+  height: number
+  width: number
+}
