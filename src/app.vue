ab36eeefa57c69480fa2cb8fa1a79f8d4298f5e7
Dan <danker.twist@gmail.com>
2023-12-03 02:15:38 -0500

    feat: added Nuxt loading indicator

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,8 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div>
+    <NuxtLoadingIndicator color="repeating-linear-gradient(to right,var(--bs-primary) 0%,var(--bs-primary) 100%)" />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
