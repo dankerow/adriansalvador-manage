75db7d953f853ad8311994af5f15ae8d263f7e57
Dan <danker.twist@gmail.com>
2024-03-15 22:06:21 -0400

    feat: add grid component

---
+++ src/components/Grid.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  items: any[]
+}>()
+</script>
+
+<template>
+  <div>
+    <div class="row row-cols-sm-2 row-cols-lg-5 g-4 mb-4">
+      <div v-for="(item, index) in items" :key="index" class="col">
+        <slot :item="item" />
+      </div>
+    </div>
+  </div>
+</template>
