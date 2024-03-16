64d9e410d50b0d7ba3af622886b8f0b3a8fd9da8
Dan <danker.twist@gmail.com>
2024-03-16 17:33:06 -0400

    feat: add file overview page

---
+++ src/pages/media/files/[id].vue
@@ -0,0 +1,121 @@
+<script setup lang="ts">
+import type { AlbumFile } from '@/types/album'
+import type { File } from '@/types/file'
+
+import { useFilesStore } from '@/stores/files'
+import { useAlbumsStore } from '@/stores/albums'
+
+const { addToast } = useToast()
+const route = useRoute()
+const filesStore = useFilesStore()
+const albumsStore = useAlbumsStore()
+
+const { data: file } = await useAsyncData<File | AlbumFile | null>(
+    `file-${route.params.id}`,
+    () => filesStore.getFile(route.params.id, { includeAlbum: true }),
+    {
+      default: () => filesStore.files.find(file => file.id === route.params.id) || null
+    }
+)
+
+if (!file.value) {
+  throw createError({ statusCode: 404, message: 'The file you are looking for couldn\'t be found.' })
+}
+
+const { data: albumFiles } = await useAsyncData(
+  `file-${route.params.id}-album-files`,
+  () => albumsStore.getAlbumFiles(file.value!.albumId!, -1),
+  {
+    immediate: file.value!.albumId !== undefined,
+    default: () => ref({ images: [], count: 0, pages: 0 }),
+    lazy: true,
+    deep: false
+  }
+)
+
+const isRootPath = computedEager(() => route.name?.startsWith('media-files-id'))
+
+const deleteFile = async () => {
+  try {
+    await filesStore.deleteFile(file.value!.id)
+
+    addToast({
+      title: 'Notification',
+      body: `The file (${file.value!.name}) was successfully deleted.`
+    })
+  } catch (error) {
+    addToast({
+      title: 'Error',
+      body: `The file (${file.value!.name}) couldn't be deleted.`
+    })
+  }
+}
+
+const previousFile = async () => {
+  if (file.value!.albumId) {
+    const index = albumFiles.value.images.findIndex((albumFile: AlbumFile) => albumFile.id === file.value!.id)
+
+    if (index !== undefined && index > 0) {
+      const previousFile = albumFiles.value.images[index - 1]
+
+      await navigateTo(`/media/files/${previousFile.id}`)
+    }
+  }
+}
+
+const nextFile = async () => {
+  if (file.value!.albumId) {
+    const index = albumFiles.value.images.findIndex((albumFile: AlbumFile) => albumFile.id === file.value!.id)
+
+    if (index !== undefined && index < albumFiles.value.images.length - 1) {
+      const nextFile = albumFiles.value.images[index + 1]
+
+      await navigateTo(`/media/files/${nextFile.id}`)
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <Banner
+      v-if="isRootPath"
+      :title="file!.name"
+      :links="[
+        { name: 'Media', path: '/media' },
+        ...file!.albumId ? [
+          { name: 'Albums' },
+          { name: file!.album?.name, path: `/media/albums/${file!.albumId}` }
+        ] : [],
+        { name: 'Files' },
+        { name: file!.name }
+      ]"
+      :buttons="[
+        ...!route.name.startsWith('media-files-id-edit') ? [{ name: 'edit', text: 'Edit', url: `/media/files/${file!.id}/edit`, icon: 'ic:twotone-edit' }] : [],
+        { name: 'delete', text: 'Delete', callback: deleteFile, icon: 'ic:twotone-delete' }
+      ]"
+    />
+
+    <NuxtPage />
+
+    <div class="container-fluid">
+      <div v-if="file!.album" class="d-flex justify-content-between mt-4">
+        <div class="col">
+          <a type="button" @click.prevent="previousFile">
+            <Icon name="ic:twotone-keyboard-arrow-left" />
+
+            Previous file in the album
+          </a>
+        </div>
+
+        <div class="col text-end">
+          <a type="button" @click.prevent="nextFile">
+            Next file in the album
+
+            <Icon name="ic:twotone-keyboard-arrow-right" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/pages/media/files/[id]/index.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts">
+import type { AlbumFile } from '@/types/album'
+import type { File } from '@/types/file'
+
+import { useFilesStore } from '@/stores/files'
+
+import PhotoSwipeLightbox from 'photoswipe/lightbox'
+import 'photoswipe/photoswipe.css'
+
+const route = useRoute()
+const filesStore = useFilesStore()
+const cdnBaseUrl = useRuntimeConfig().public.cdnBaseUrl
+
+const { data: file } = await useAsyncData<File | AlbumFile | null>(
+    `file-${route.params.id}`,
+    () => filesStore.files.find(file => file.id === route.params.id) || filesStore.getFile(route.params.id, { includeAlbum: true }),
+    {
+      default: () => null
+    }
+)
+
+if (!file.value) {
+  throw createError({ statusCode: 404, message: 'The file you are looking for couldn\'t be found.' })
+}
+
+const lightbox = ref<PhotoSwipeLightbox | null>(null)
+
+const getImageView = computed<string>(() => {
+  return `${cdnBaseUrl}/s-files/${encodeURIComponent(file.value!.name)}`
+})
+
+onMounted(() => {
+  if (!lightbox.value) {
+    lightbox.value = new PhotoSwipeLightbox({
+      gallery: '#preview-image',
+      children: 'a',
+      pswpModule: () => import('photoswipe'),
+      wheelToZoom: true
+    })
+
+    lightbox.value?.init()
+  }
+})
+
+onUnmounted(() => {
+  if (lightbox.value !== null) {
+    lightbox.value?.destroy()
+    lightbox.value = null
+  }
+})
+</script>
+
+<template>
+  <div class="container-fluid">
+    <div class="card mb-5">
+      <div class="card-body">
+        <div class="row row-cols-2 g-4">
+          <div id="preview-image" class="col-auto">
+            <a
+              :href="getImageView"
+              :data-pswp-width="file!.metadata.width"
+              :data-pswp-height="file!.metadata.height"
+              rel="noreferrer"
+              aria-label="View image"
+            >
+              <nuxt-img
+                class="img-fluid rounded"
+                format="webp"
+                :src="getImageView"
+                :width="300"
+                loading="lazy"
+                decode="async"
+                draggable="false"
+                :alt="file!.name"
+              />
+            </a>
+          </div>
+
+          <div class="col-lg-4">
+            <h6 class="fw-bold mb-0">
+              Name
+            </h6>
+
+            <p class="text-body-secondary">
+              {{ file!.name }}
+            </p>
+
+            <h6 class="fw-bold mb-0">
+              Type
+            </h6>
+
+            <p class="text-body-secondary">
+              {{ file!.type }}
+            </p>
+
+            <h6 class="fw-bold mb-0">
+              Size
+            </h6>
+
+            <p class="text-body-secondary">
+              {{ file!.size }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
