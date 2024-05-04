70958dcd759fa14192220e3a1808ff19f488f34b
Dan <danker.twist@gmail.com>
2024-05-04 10:17:41 -0400

    feat: fetch albums and files on nuxt server initialization

--- src/plugins/init.server.ts
+++ src/plugins/init.server.ts
@@ -1,11 +1,13 @@
 import { useAuthStore } from '@/stores/auth'
-import { useAlbumsStore } from '@/stores/albums'
 import { useSettingsStore } from '@/stores/settings'
+import { useAlbumsStore } from '@/stores/albums'
+import { useFilesStore } from '@/stores/files'
 
 export default defineNuxtPlugin(async (nuxtApp) => {
   const authStore = useAuthStore(nuxtApp.$pinia)
-  const albumsStore = useAlbumsStore(nuxtApp.$pinia)
   const settingsStore = useSettingsStore(nuxtApp.$pinia)
+  const filesStore = useFilesStore(nuxtApp.$pinia)
+  const albumsStore = useAlbumsStore(nuxtApp.$pinia)
 
   const token = useCookie('token')
   const sidebar = useCookie('sidebar')
@@ -16,7 +18,8 @@ export default defineNuxtPlugin(async (nuxtApp) => {
   try {
     await Promise.all([
       authStore.verify(),
-      albumsStore.getAlbums()
+      filesStore.getFiles({ includeAlbum: true }, { setData: true }),
+      albumsStore.getAlbums({}, { setData: true })
     ])
   } catch (e) {
     authStore.logout()
