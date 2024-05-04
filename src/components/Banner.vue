2e19b86ea9286a1f02ac385525dae2c89940539e
Dan <danker.twist@gmail.com>
2024-05-04 10:14:50 -0400

    feat: add possibility to edit breadcrumb links

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -8,10 +8,17 @@ const props = withDefaults(defineProps<{
     url?: string
     disabled?: boolean
     callback?:() => void
+  }[],
+  links?: {
+    name: string
+    path?: string
   }[]
 }>(), {
-  buttons: () => ([])
+  buttons: () => ([]),
+  links: () => ([])
 })
+
+const route = useRoute()
 
 const buttons = computed(() => {
   return props.buttons?.map((button) => {
@@ -32,13 +39,62 @@ const buttons = computed(() => {
     <div class="banner py-4">
       <div class="container-fluid">
         <div class="row align-items-center">
-          <div class="col-12 col-md-6 col-lg-4">
-            <h1 class="fw-bolder d-inline-block mb-0">
+          <div class="col-12 col-md-6 col-lg-6">
+            <h3 class="fw-bolder text-white mb-3">
               {{ title }}
-            </h1>
+            </h3>
+
+            <nav
+              class="d-inline-block"
+              aria-label="breadcrumb"
+            >
+              <ol class="breadcrumb bg-dark bg-opacity-75 rounded-3 mb-0 px-3 py-1 align-items-center text-capitalize">
+                <li class="breadcrumb-item">
+                  <NuxtLink
+                    class="link-body-emphasis"
+                    to="/dashboard"
+                    :disabled="route.path === '/dashboard'"
+                  >
+                    home
+                  </NuxtLink>
+                </li>
+
+                <template
+                  v-for="link in links"
+                  :key="link.name"
+                >
+                  <li
+                    v-if="link.path && link.path !== route.path"
+                    class="breadcrumb-item"
+                  >
+                    <NuxtLink
+                      class="link-body-emphasis fw-semibold"
+                      :class="{
+                        'text-truncate': link.name.length > 20
+                      }"
+                      :to="link.path"
+                      :disabled="link.path === route.path"
+                    >
+                      {{ link.name }}
+                    </NuxtLink>
+                  </li>
+
+                  <li
+                    v-else
+                    class="breadcrumb-item active"
+                    aria-current="page"
+                  >
+                    {{ link.name }}
+                  </li>
+                </template>
+              </ol>
+            </nav>
           </div>
 
-          <div v-if="buttons?.length" class="col-12 col-md-6 col-lg-8 text-end">
+          <div
+            v-if="buttons?.length"
+            class="col-12 col-md-6 col-lg-6 text-end"
+          >
             <slot
               v-for="(button, index) in buttons"
               :key="index"
@@ -53,7 +109,10 @@ const buttons = computed(() => {
                 :class="{ disabled: button.disabled }"
                 :disabled="button.disabled"
               >
-                <Icon v-if="button.icon" :name="button.icon" />
+                <Icon
+                  v-if="button.icon"
+                  :name="button.icon"
+                />
                 <span>
                   {{ button.text }}
                 </span>
@@ -62,12 +121,15 @@ const buttons = computed(() => {
               <button
                 v-else
                 :key="button.name"
-                class="btn btn-sm btn-white"
+                class="btn btn-sm btn-white d-inline-flex align-items-center"
                 :class="{ disabled: button.disabled }"
                 :disabled="button.disabled"
                 @click="button.callback"
               >
-                <Icon v-if="button.icon" :name="button.icon" />
+                <Icon
+                  v-if="button.icon"
+                  :name="button.icon"
+                />
                 <span>
                   {{ button.text }}
                 </span>
