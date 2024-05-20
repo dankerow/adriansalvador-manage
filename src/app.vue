b61184d3c74a98b754bdf6b6915897584429d559
Dan <danker.twist@gmail.com>
2024-05-20 02:09:28 -0700

    chore: use @nuxt/scripts for script loading

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+useScriptNpm({
+  packageName: 'bootstrap',
+  file: 'dist/js/bootstrap.min.js',
+  version: '5.3.2',
+  scriptOptions: {
+    bundle: true
+  }
+})
+</script>
+
 <template>
   <div>
     <NuxtLoadingIndicator color="repeating-linear-gradient(to right,var(--bs-primary) 0%,var(--bs-primary) 100%)" />
