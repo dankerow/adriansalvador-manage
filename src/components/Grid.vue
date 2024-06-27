485f6ed2a58a5fdee553d5a47def849489775a15
Dan <danker.twist@gmail.com>
2024-06-26 20:47:28 -0700

    feat: make columns number customizable

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-defineProps<{
+withDefaults(defineProps<{
   items: never[]
-}>()
+  columns: number
+}>(), {
+  columns: 5
+})
 </script>
 
 <template>
   <div>
-    <div class="row row-cols-sm-2 row-cols-lg-5 g-4 mb-4">
+    <div :class="`row row-cols-1 row-cols-sm-2 row-cols-lg-${columns} g-4 mb-4`">
       <div v-for="(item, index) in items" :key="index" class="col">
         <slot :item="item" />
       </div>
